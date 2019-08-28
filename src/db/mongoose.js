e0955ac0ec5104c607b8d01a5ca53cb016094fbe
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-28 22:31:06 +0200

    added model task to user collection

---
+++ src/db/mongoose.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+});
+// const me = new User({
+//     name: 'Davi',
+//     age: 'mike'
+// });
+// me.save().then(() => {
+//     console.log(me)
+
+// }).catch((error) => {
+//     console.log('Error ', error);
+// })
+const Task = mongoose.model('Task', {
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    }
+});
+const task = new Task({
+    description: 'throw the trash',
+    completed: false
+})
+task.save().then((task) => {
+    console.log(task);
+}).catch((error) => {
+    console.log('Error ', error)
+})
