1c4eeee0cae2917d012e637e99232f6b4372470f
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-29 22:15:28 +0200

    using  validator module with email.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
@@ -6,6 +8,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
     name: {
         type: String
+    },
+    email: {
+        type: String,
+        required: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        }
     },
     age: {
         type: Number,
@@ -18,7 +29,7 @@ const User = mongoose.model('User', {
 });
 const me = new User({
     name: 'Davi',
-    age: -1
+    email: 'davi@'
 });
 me.save().then(() => {
     console.log(me)
