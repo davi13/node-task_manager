d7b54069f450b77d72cfacefc2be4bd30f15b3ff
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-31 20:50:07 +0200

    added the model/user bd

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,33 +5,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 });
-// Users
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid');
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number');
-            }
-        }
-    }
-});
+
 // const me = new User({
 //     name: ' eli ',
 //     email: 'eli@eli.com'
---
+++ src/models/user.js
@@ -0,0 +1,28 @@
+// Users
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number');
+            }
+        }
+    }
+});
+module.exports = user
