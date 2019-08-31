ce2e44eb2767d931760cb526022ac80f2b71849a
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-31 20:22:18 +0200

    uging postman to fecth info from  express to get access to the end point

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.post('/users', (req, res) => {
+    res.send('Testing !')
+
+});
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
