f26b324ba4e1582902c958f9d6285711cbe8d75d
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-08-31 20:47:42 +0200

    added the model folder for mongosse to moduling the conection to the mongodb

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.post('/users', (req, res) => {
+    console.log(req.body);
     res.send('Testing !')
 
 });
