bca8408850b337d5766bb32e2c4f6d528e6d7a70
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-02 11:59:46 +0200

    using async and await instead of promises

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,11 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndRemove('5d6b5d4efdf531d70d7ff5ea', { completed: true }).then((task) => {
+Task.findByIdAndDelete('5d6b5d4efdf531d70d7ff5ea', { completed: true }).then((task) => {
     console.log(task);
     return Task.countDocuments({ completed: false });
 }).then((result) => {
     console.log(result);
 }).catch((e) => {
     console.log(e);
-})
+});
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -8,5 +8,17 @@ User.findByIdAndUpdate('5d683983189170c97af5a5b8', { age: 1 }).then((user) => {
     console.log(result);
 }).catch((e) => {
     console.log(e)
+});
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age });
+    const count = await User.countDocuments({ age: 2 })
+    return count;
+}
+
+updateAgeAndCount('5d683983189170c97af5a5b8', 1).then((count) => {
+    console.log(count);
+}).catch((e) => {
+    console.log(e);
 })
 
