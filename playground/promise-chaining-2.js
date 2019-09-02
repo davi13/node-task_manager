3d1b7e83719fe41e57788f0695f4fa957e230957
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-02 12:27:42 +0200

    promise-chaining-2.js:  using async and await instead promises

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,22 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('5d6b5d4efdf531d70d7ff5ea', { completed: true }).then((task) => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-}).then((result) => {
-    console.log(result);
+// Task.findByIdAndDelete('5d6b5d4efdf531d70d7ff5ea', { completed: true }).then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+// }).then((result) => {
+//     console.log(result);
+// }).catch((e) => {
+//     console.log(e);
+// });
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({ completed: false });
+    return count
+}
+deleteTaskAndCount('5d6ced6ccd0b9fdfda04ce5b').then((count) => {
+    console.log(count);
 }).catch((e) => {
     console.log(e);
-});
+})
