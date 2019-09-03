5de0ca83e41380a1b40eac6f180342b421bb4ce5
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-03 19:12:32 +0200

    index.js: detete an task  with findByIdAndDelete

--- src/index.js
+++ src/index.js
@@ -79,7 +79,7 @@ app.patch('/users/:id', async (req, res) => {
     }
 });
 
-app.delete('/user/:id', (req, res) => {
+app.delete('/user/:id', async (req, res) => {
     try {
         const user = await User.findByIdAndDelete(req.params.id);
         if (!user) {
@@ -162,12 +162,19 @@ app.patch('/task/:id', async (req, res) => {
         res.status(500).send(e);
     }
 
-})
+});
 
-
-
-
-
+app.delete('/task/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if (!task) {
+            return res.status(404).send();
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
