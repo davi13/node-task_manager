f94cb05349bd188b71d5f642e51088c3b132ee87
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-03 20:16:01 +0200

    src/router/task.js: creation of router for the task

--- src/index.js
+++ src/index.js
@@ -9,92 +9,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(userRouter)
+app.use(userRouter);
+app.use(taskRouter);
 
 
-app.post('/task', async (req, res) => {
-    try {
-        const task = new Task(req.body);
-        await task.save();
-        res.status(201).send(task);
-    } catch (e) {
-        res.status(400).send(error);
-    }
-    // task.save().then(() => {
-    //     res.status(201).send(task);
-    // }).catch((error) => {
-    //     res.status(400).send(error);
-    // });
-});
 
-app.get('/task', async (req, res) => {
-    try {
-        const tasks = await Task.find({});
-        res.send(tasks);
-    } catch (e) {
-        res.status(500).send();
-    }
-    // Task.find({}).then((task) => {
-    //     res.send(task);
-    // }).catch((error) => {
-    //     res.status(500).send();
-    // })
-});
-
-
-app.get('/task/:id', async (req, res) => {
-    const _id = req.params.id
-    try {
-        const task = await Task.findById(_id);
-        if (!task) {
-            return res.status(404).send();
-        }
-        res.send(task);
-    } catch (e) {
-        res.status(500).send(error);
-    }
-
-    // Task.findById(_id).then((task) => {
-    //     if (!task) {
-    //         return res.status(404).send();
-    //     }
-    //     res.send(task);
-    // }).catch((error) => {
-    //     res.status(500).send(error)
-    // });
-});
-
-app.patch('/task/:id', async (req, res) => {
-    const updates = Object.keys(req.body);
-    const allowedUpdtes = ['description', 'completed'];
-    const isvalideOperation = updates.every((update) => allowedUpdtes.includes(update));
-    if (!isvalideOperation) {
-        return res.status(400).send({ error: 'Invalid Updates' });
-    }
-
-    try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-        if (!task) {
-            res.status(404).send()
-        }
-        res.send(task);
-    } catch (e) {
-        res.status(500).send(e);
-    }
-
-});
-
-app.delete('/task/:id', async (req, res) => {
-    try {
-        const task = await Task.findByIdAndDelete(req.params.id);
-        if (!task) {
-            return res.status(404).send();
-        }
-        res.send(task)
-    } catch (e) {
-        res.status(500).send(e);
-    }
-});
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
