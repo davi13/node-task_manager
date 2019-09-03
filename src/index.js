b6790aa04edb5150aa5a07a59acb536541a5d71c
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-03 18:49:00 +0200

    index.js: detete an user with findByIdAndDelete

--- src/index.js
+++ src/index.js
@@ -77,7 +77,20 @@ app.patch('/users/:id', async (req, res) => {
     } catch (e) {
         res.status(500).send(e)
     }
-})
+});
+
+app.delete('/user/:id', (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (e) {
+        res.status(500).send(e);
+
+    }
+});
 
 app.post('/task', async (req, res) => {
     try {
