9b56f95ad268029462803c4b6ead43bb60dc6d96
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-04 22:54:40 +0200

    index.js: using costum middleware

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 require('./db/mongoose');
-const User = require('./models/user');
-const Task = require('./models/task');
 const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task');
 
+
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+// app.use((req, res, next) => {
+//     console.log(req.method, req.path);
+//     next();
+// })
+
+app.use((req, res, next) => {
+    if (req.method === 'GET' || req.method === 'POST' || req.method === 'PATCH' || req.method === 'DELETE') {
+        res.status(503).send('message');
+    } else {
+        next();
+    }
+})
 
 app.use(express.json());
 app.use(userRouter);
