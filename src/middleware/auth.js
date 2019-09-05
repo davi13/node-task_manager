39d7dbeffefd02f572312a9ab667d7e6eee4034e
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-05 16:56:49 +0200

    src/middleware: creation of the function for authentificate the user

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,7 @@ const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '');
         const decoded = jwt.verify(token, 'thisismynewcourse');
-        const user = await User.findOne({ _id: decoded._id, 'tokens.tokens': token });
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
         if (!user) {
             throw new Error();
         }
