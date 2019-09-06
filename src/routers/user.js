4d813e8f1419ee782a606cc3eb38eafe6aa2fc1d
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-06 20:27:12 +0200

    routers/user.js:using the middelware to delete our own acount, identificatin by token

--- src/routers/user.js
+++ src/routers/user.js
@@ -55,20 +55,7 @@ router.get('/users/me', auth, async (req, res) => {
     //     res.status(500).send();
     // }
 });
-//Searching User by Id
-router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id;
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (e) {
-        res.status(500).send(e)
 
-    }
-});
 //Updateing User info in the user 
 router.patch('/users/:id', async (req, res) => {
     const updates = Object.keys(req.body);
@@ -92,9 +79,9 @@ router.patch('/users/:id', async (req, res) => {
     }
 });
 //Ereasing user
-router.delete('/user/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id);
+        const user = await User.findByIdAndDelete(req.user._id);
         if (!user) {
             return res.status(404).send();
         }
