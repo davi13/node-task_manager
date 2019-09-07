7e7eae719c1838397fb7d0c74de74268fb409276
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-07 21:42:33 +0200

    router/task.js:getting all the task made by user

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,10 +18,10 @@ router.post('/task', auth, async (req, res) => {
     }
 });
 
-router.get('/tasks', async (req, res) => {
+router.get('/tasks/:id', auth, async (req, res) => {
     try {
-        const tasks = await Task.find({});
-        res.send(tasks);
+        await req.user.populate('tasks').execPopulate();
+        res.send(req.user.tasks);
     } catch (e) {
         res.status(500).send();
     }
