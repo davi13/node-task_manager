f516cb19035ab9728c65d0370ee3055d36f1b87a
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-07 21:58:17 +0200

    router/task.js:updating one task made by user

--- src/routers/task.js
+++ src/routers/task.js
@@ -20,9 +20,11 @@ router.post('/task', auth, async (req, res) => {
 
 router.get('/tasks/:id', auth, async (req, res) => {
 
-    const tasks = await Task.find({ owner: req.user._id });
+    //const tasks = await Task.find({ owner: req.user._id });
+
     try {
-        res.send(tasks);
+        await req.user.populate('tasks').execPopulate();
+        res.send(req.user.tasks);
     } catch (e) {
         res.status(500).send();
     }
@@ -43,7 +45,7 @@ router.get('/task/:id', auth, async (req, res) => {
     }
 });
 
-router.patch('/task/:id', async (req, res) => {
+router.patch('/task/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body);
     const allowedUpdtes = ['description', 'completed'];
     const isvalideOperation = updates.every((update) => allowedUpdtes.includes(update));
@@ -52,12 +54,14 @@ router.patch('/task/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findByIdAndUpdate(req.params.id);
-        updates.forEach((update) => task[update] = req.body[update]);
-        await task.save();
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+        //const task = await Task.findByIdAndUpdate(req.params.id);
+
         if (!task) {
             res.status(404).send()
         }
+        updates.forEach((update) => task[update] = req.body[update]);
+        await task.save();
         res.send(task);
     } catch (e) {
         res.status(500).send(e);
