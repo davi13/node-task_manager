694c0811438993dcd3d858be9041e6225a678059
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-08 21:58:03 +0200

    sorting completed using method sortBy completed: true, completed : false

--- src/routers/task.js
+++ src/routers/task.js
@@ -28,8 +28,12 @@ router.get('/tasks', auth, async (req, res) => {
     const sort = {};
     if (req.query.completed) {
         match.completed = req.query.completed === 'true';
-
     }
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
     try {
         await req.user.populate({
             path: 'tasks',
