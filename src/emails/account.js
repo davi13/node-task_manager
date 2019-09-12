e24842402e07091667d5e98e4a43d281845f09f9
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-12 20:29:40 +0200

    emails/account.js: seding our first email

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,1 +1,11 @@
+const sgMail = require('@sendgrid/mail');
 const sendgridAPIKey = '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';
+
+sgMail.setApiKey(sendgridAPIKey);
+
+sgMail.send({
+    to: 'dante1310@hotmail.com',
+    from: 'dante1310@hotmail.com',
+    subject: 'this is my first creation',
+    text: 'I hope this one actually get to you.'
+})
