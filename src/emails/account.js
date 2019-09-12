e8be336178c3c658a9985cb553b5948119fb826e
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-12 20:46:22 +0200

    emails/account.js: changed the mail adres to another, it working

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,7 +4,7 @@ const sendgridAPIKey = 'SG.iJ0ynI0kRBehfgox17YNKg.twNwFfBNqJYMb-AWkQexfrreO95d29
 sgMail.setApiKey(sendgridAPIKey);
 
 sgMail.send({
-    to: 'dante1310@hotmail.com',
+    to: 'cia1310@homatil.fr',
     from: 'dante1310@hotmail.com',
     subject: 'this is my first creation',
     text: 'I hope this one actually get to you.'
