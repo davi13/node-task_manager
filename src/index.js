1fad5e768deadc331c5096e8d5327270d9f9ded4
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-12 19:15:05 +0200

    added sharp module to re-sizing the avatar img

--- src/index.js
+++ src/index.js
@@ -8,35 +8,9 @@ const taskRouter = require('./routers/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//     console.log(req.method, req.path);
-//     next();
-// })
-
-// app.use((req, res, next) => {
-
-//     res.status(503).send('Site is curently daow. check back soon !');
-
-// })
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
-const multer = require('multer');
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    }, fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(doc|docx)$/)) {
-            return cb(new Error('Please upload a PDF'));
-        }
-        cb(undefined, true);
-        // cb(new Error('file must be a PDF'));
-        // cb(undefined, true);
-        // cb(undefined, false);
-    }
-});
 
 
 
@@ -45,9 +19,6 @@ app.post('/upload', upload.single('upload'), (req, res) => {
 }, (error, req, res, next) => {
     res.status(400).send({ error: error.message })
 })
-
-
-
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
