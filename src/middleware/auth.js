579cfe23bb600f100087ae3311753de1d1d85fef
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-12 16:09:23 +0200

    I was looking for a solution to the token in the middleware cause there is a conflit with the tokenfrom the mobodb. when the user is login in imposible to set the  avatar for the user. I had trying many solution with Postman, but the only it working is to use bearek token  instead of inherit auth from parent. normally they has to take the token placed in variable on postman

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+
 const auth = async (req, res, next) => {
     try {
         const token = req.header('Authorization').replace('Bearer ', '')
@@ -13,7 +14,7 @@ const auth = async (req, res, next) => {
         req.user = user
         next()
     } catch (e) {
-        res.status(401).send({ error: 'Please authenticate.' })
+        res.status(401).send({ error: 'Please authenticate please.' })
     }
 }
 
