86258add5242480c7b6245b542fecaef9ce12906
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-13 20:05:15 +0200

    removing a a information

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,8 +4,8 @@ const sendgridAPIKey = '';
 sgMail.setApiKey(sendgridAPIKey);
 
 sgMail.send({
-    to: 'cia1310@homatil.fr',
-    from: 'dante1310@hotmail.com',
+    to: '',
+    from: '',
     subject: 'this is my first creation',
     text: 'I hope this one actually get to you.'
 })
