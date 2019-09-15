a2359728e70fd764c7abe341d285997b15374c04
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-15 22:14:27 +0200

    maked some test to one of our endpoint

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,1 +1,10 @@
-const resquest = require('supertest');
+const request = require('supertest');
+const app = require('../src/app');
+
+test('Should signup a new user', async () => {
+    await request(app).post('/users').send({
+        name: 'Picollo',
+        email: 'davie0@xample.com',
+        password: 'D123456'
+    }).expect(201)
+});
