66da7e73f44703d6b5f87e63936aa2bdd17d0f4e
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-15 21:45:50 +0200

    maked some refactoring in the index.js  and creation of app.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
-const express = require('express');
-require('./db/mongoose');
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
+const app = require('./app');
 
-
-
-const app = express();
 const port = process.env.PORT;
-
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
-
-
-
-// app.post('/upload', upload.single('upload'), (req, res) => {
-//     res.send();
-// }, (error, req, res, next) => {
-//     res.status(400).send({ error: error.message })
-// })
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
 });
+
+
+
 
---
+++ tests/user.test.js
@@ -0,0 +1,1 @@
+const resquest = require('supertest');
