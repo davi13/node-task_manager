8deea414db6eab317220b64d85630b3cfff0ee66
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-16 11:54:23 +0200

    added a test to login to the app and make unintentionally a test with worng info to test

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,9 +2,16 @@ const request = require('supertest');
 const app = require('../src/app');
 const User = require('../src/models/user');
 
+const userOne = {
+    name: 'Broly',
+    email: 'davie1@xample.com',
+    password: 'D123456'
+}
+
 beforeEach(async () => {
     await User.deleteMany();
-})
+    await new User(userOne).save();
+});
 
 
 test('Should signup a new user', async () => {
@@ -14,3 +21,17 @@ test('Should signup a new user', async () => {
         password: 'D123456'
     }).expect(201);
 });
+
+test('Should login existing user', async () => {
+    await request(app).post('/users/login').send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200);
+});
+
+test('Should not login nonexistent user', async () => {
+    await request(app).post('/users/login').send({
+        email: 'mywrong@email.com',
+        password: 'notTheRight'
+    }).expect(400)
+})
