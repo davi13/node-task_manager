3099a206e2bc2adc7dd93501a34ee3236c3fb220
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-17 20:56:45 +0200

    tests/task.js : creation of test for the task that each user can add V2

--- tests/task.test.js
+++ tests/task.test.js
@@ -6,5 +6,14 @@ const { userOneId, userOne, setupDatabase } = require('./fixtures/db');
 
 beforeEach(setupDatabase);
 test('Should creat task for user', async () => {
-
-})
+    const response = await request(app)
+        .post('/tasks')
+        .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: 'from my test'
+        })
+        .expect(201);
+    const task = await Task.findById(response.body._id);
+    expect(task).not.toBeNull();
+    expect(task.completed).toEqual(false);
+});
