6de91d9ec39ce4c08d9e0ddec0e69b630ab4add9
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-09-18 21:02:28 +0200

    tests/task.js: Should not userTwo delete a task for userOne V3

--- tests/task.test.js
+++ tests/task.test.js
@@ -13,6 +13,7 @@ const {
     setupDatabase } = require('./fixtures/db');
 
 beforeEach(setupDatabase);
+
 test('Should creat task for user', async () => {
     const response = await request(app)
         .post('/tasks')
@@ -23,8 +24,8 @@ test('Should creat task for user', async () => {
         .expect(201);
 
     const task = await Task.findById(response.body._id)
-        .expect(task).not.toBeNull()
-        .expect(task.completed).toEqual(false);
+    expect(task).not.toBeNull()
+    expect(task.completed).toEqual(false);
 });
 
 test('Should fetch user tasks', async () => {
@@ -33,7 +34,7 @@ test('Should fetch user tasks', async () => {
         .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
         .send()
         .expect(200)
-        .expect(response.body.length).toEqual(2);
+    expect(response.body.length).toEqual(2);
     //task = await response.body
     // expect(task).not.toBeNull();
     // expect(task.length).toEqual(2);
@@ -45,5 +46,7 @@ test('Should not delete other users task', async () => {
         .delete(`/tasks/${taskOne._id}`)
         .set('Authorization', `Bearer ${userTwo.tokens[0].token}`)
         .send()
-        .expect(400);
+        .expect(404);
+    const task = await Task.findById(taskOne._id)
+    expect(task).not.toBeNull()
 }) 
